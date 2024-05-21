db2fb20f65a9cd6c068e9a9d7a481967f74858d8
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-05-22 05:56:12 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yoochan
+// Created on: May 2024
 // This file contains the JS functions for index.html
+function fibonacci() { // without recursive function and list.
+  const userNumber = document.getElementById("get-number").value
+  let fibonacciNumber1 = 1 //F1     F1 + F2 = 1, Fn-1 + Fn-2 = Fn
+  let fibonacciNumber2 = 0
+  let savePoint = 0
+  for(counter=1;counter<userNumber;counter++) {
+    savePoint = fibonacciNumber1
+    fibonacciNumber1 = fibonacciNumber2 + fibonacciNumber1
+    fibonacciNumber2 = savePoint
+  }
+  const answer = fibonacciNumber1
+document.getElementById("TheAnswer").innerHTML = "In the Fibonacci sequence, the " + userNumber + "th number is " + answer
+}
